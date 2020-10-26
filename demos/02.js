775f1cadd3e66e3ad6008eca636c0a6f4b187284
qmliu <qmliu@pearlinpalm.com>
2020-10-26 13:38:27 +0800

    update files

--- demos/02.js
+++ demos/02.js
@@ -2,7 +2,7 @@ const Koa = require('koa');
 const app = new Koa();
 
 const main = ctx => {
-  ctx.response.body = 'Hello World';
+  ctx.response.body = '这是真的不错的docker 啊ele Hello World';
 };
 
 app.use(main);
---
+++ entrypoint.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+#用于启动容器时运行
+
+npm install --registry=https://registry.npm.taobao.org
+node demos/02.js
